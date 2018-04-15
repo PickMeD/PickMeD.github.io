9eda344fc72554409bb565e053bc52bcf3194534
giveme <giveme@github.com>
2018-04-15 15:20:59 +0900

    픽미 앱 , 디렉토리 메타데이타 read & write

---
+++ Android/pickme/app/src/main/java/com/example/a1002732/pickme/CertificateActivity.java
@@ -0,0 +1,18 @@
+package com.example.a1002732.pickme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by 1002732 on 2018. 4. 15..
+ */
+
+public class CertificateActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.certificate);
+
+    }
+}
---
+++ Android/pickme/app/src/main/java/com/example/a1002732/pickme/PickMeActivity.java
@@ -0,0 +1,17 @@
+package com.example.a1002732.pickme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by 1002732 on 2018. 4. 15..
+ */
+
+public class PickMeActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pickme);
+    }
+}
